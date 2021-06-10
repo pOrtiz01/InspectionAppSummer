9f8b7605366b35040aaa7818779349b8f2d2e4b9
pabor <18plog@queensu.ca>
2021-06-10 13:07:04 -0400

    Edit Profile Activity 2

--- app/src/main/java/com/example/myapplication/ui/notifications/NotificationsFragment.java
+++ app/src/main/java/com/example/myapplication/ui/notifications/NotificationsFragment.java
@@ -6,7 +6,6 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -14,7 +13,6 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
-import com.example.myapplication.EditProfile;
 import com.example.myapplication.R;
 import com.example.myapplication.Register;
 import com.example.myapplication.databinding.FragmentNotificationsBinding;
