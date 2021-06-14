241ec233e31c4ee2e7bf20db743d4411a0a6a726
pabor <18plog@queensu.ca>
2021-06-14 13:38:56 -0400

    Finished phone formatting

--- app/src/main/java/com/example/myapplication/createInspection.java
+++ app/src/main/java/com/example/myapplication/createInspection.java
@@ -12,6 +12,6 @@ public class createInspection extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_inspection);
         Button mybutton= new Button(this);
-        
+
     }
 }
